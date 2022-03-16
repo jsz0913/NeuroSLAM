88d6e5511813e83c96bf784110ad914ca6ef4765
jsz0913 <66623990+jsz0913@users.noreply.github.com>
2022-03-16 19:35:53 +0800

    Update get_gc_xyz.m

--- 01_conjunctive_pose_cells_network/3d_grid_cells_network/get_gc_xyz.m
+++ 01_conjunctive_pose_cells_network/3d_grid_cells_network/get_gc_xyz.m
@@ -3,14 +3,13 @@ function [gcX, gcY, gcZ] = get_gc_xyz()
 
     % The 3D Grid Cells Network 三维矩阵
     global GRIDCELLS;
-    
-    % The x, y, z dimension of 3D Grid Cells Model (3D CAN) 
     global GC_X_DIM;
     global GC_Y_DIM;
     global GC_Z_DIM;
 
     % The x, y, z theta size of each unit in radian, 2*pi/ x_dim
     % radian e.g. 2*pi/36 = 0.175
+    % x y z 都是角度？？
     global GC_X_TH_SIZE;   
     global GC_Y_TH_SIZE;   
     global GC_Z_TH_SIZE;   
@@ -21,8 +20,10 @@ function [gcX, gcY, gcZ] = get_gc_xyz()
 
     global GC_X_SUM_SIN_LOOKUP;
     global GC_X_SUM_COS_LOOKUP;
+    
     global GC_Y_SUM_SIN_LOOKUP;
     global GC_Y_SUM_COS_LOOKUP;
+    
     global GC_Z_SUM_SIN_LOOKUP;
     global GC_Z_SUM_COS_LOOKUP;
 
@@ -36,33 +37,30 @@ function [gcX, gcY, gcZ] = get_gc_xyz()
     global GC_MAX_Z_WRAP;
 
 
-    % find the max activated cell
+    % find the max activated cell  x y z 的形式
     indexes = find(GRIDCELLS);
     [value, index] = max(GRIDCELLS(indexes));
     [x y z] = ind2sub(size(GRIDCELLS), indexes(index));
 
     % take the max activated cell +- AVG_CELL in 3d space
+    % 附近packet
     tempGridcells = zeros(GC_X_DIM, GC_X_DIM, GC_Z_DIM);
-
     tempGridcells(GC_MAX_X_WRAP(x : x + GC_PACKET_SIZE * 2), ...
         GC_MAX_Y_WRAP(y : y + GC_PACKET_SIZE * 2), ...
         GC_MAX_Z_WRAP(z : z + GC_PACKET_SIZE * 2)) = ...
         GRIDCELLS(GC_MAX_X_WRAP(x : x + GC_PACKET_SIZE * 2), ...
         GC_MAX_Y_WRAP(y : y + GC_PACKET_SIZE * 2), ...
         GC_MAX_Z_WRAP(z : z + GC_PACKET_SIZE * 2));
-
+    % x y z 顺序
+    % 类似python  第一个是 列向量 第二是行向量 第三个是 多个矩阵
     xSumSin = sum(GC_X_SUM_SIN_LOOKUP * sum(sum(tempGridcells,2),3));
     xSumCos = sum(GC_X_SUM_COS_LOOKUP * sum(sum(tempGridcells,2),3));
-
     ySumSin = sum(GC_Y_SUM_SIN_LOOKUP * sum(sum(tempGridcells,1),3)');
     ySumCos = sum(GC_Y_SUM_COS_LOOKUP * sum(sum(tempGridcells,1),3)');
-
     tempZSum = sum(sum(tempGridcells,1),2);
-
     for i = 1: GC_Z_DIM
         zSum(i)= tempZSum(1,1,i);
     end
-
     zSumSin = sum(GC_Z_SUM_SIN_LOOKUP * zSum');
     zSumCos = sum(GC_Z_SUM_COS_LOOKUP * zSum');
 
