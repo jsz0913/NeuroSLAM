ea3d2300352dd044119995aed15023e3d49764f3
jsz0913 <66623990+jsz0913@users.noreply.github.com>
2022-03-16 19:18:37 +0800

    Update get_gc_xyz.m

--- 01_conjunctive_pose_cells_network/3d_grid_cells_network/get_gc_xyz.m
+++ 01_conjunctive_pose_cells_network/3d_grid_cells_network/get_gc_xyz.m
@@ -1,37 +1,13 @@
 function [gcX, gcY, gcZ] = get_gc_xyz()
-%     NeuroSLAM System Copyright (C) 2018-2019 
-%     NeuroSLAM: A Brain inspired SLAM System for 3D Environments
-%
-%     Fangwen Yu (www.yufangwen.com), Jianga Shang, Youjian Hu, Michael Milford(www.michaelmilford.com) 
-%
-%     The NeuroSLAM V1.0 (MATLAB) was developed based on the OpenRatSLAM (David et al. 2013). 
-%     The RatSLAM V0.3 (MATLAB) developed by David Ball, Michael Milford and Gordon Wyeth in 2008.
-% 
-%     Reference:
-%     Ball, David, Scott Heath, Janet Wiles, Gordon Wyeth, Peter Corke, and Michael Milford.
-%     "OpenRatSLAM: an open source brain-based SLAM system." Autonomous Robots 34, no. 3 (2013): 149-176.
-% 
-%     This program is free software: you can redistribute it and/or modify
-%     it under the terms of the GNU General Public License as published by
-%     the Free Software Foundation, either version 3 of the License, or
-%     (at your option) any later version.
-% 
-%     This program is distributed in the hope that it will be useful,
-%     but WITHOUT ANY WARRANTY; without even the implied warranty of
-%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%     GNU General Public License for more details.
-% 
-%     You should have received a copy of the GNU General Public License
-%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    % The 3D Grid Cells Network
+
+    % The 3D Grid Cells Network 三维矩阵
     global GRIDCELLS;
     
     % The x, y, z dimension of 3D Grid Cells Model (3D CAN) 
     global GC_X_DIM;
     global GC_Y_DIM;
     global GC_Z_DIM;
-
 
     % The x, y, z theta size of each unit in radian, 2*pi/ x_dim
     % radian e.g. 2*pi/36 = 0.175
